914c8b8b6ce86dedd5ca028e224d4f80fe6b97ee
vasyl.bhd <vasyl.bhd@gmail.com>
2022-10-30 23:15:08 +0200

    Add new command

---
+++ src/main/java/com/zoomfolks/tidsoptimist_bot/bot/command/processor/PongCommandProcessor.java
@@ -0,0 +1,27 @@
+package com.zoomfolks.tidsoptimist_bot.bot.command.processor;
+
+import com.zoomfolks.tidsoptimist_bot.bot.command.AbstractCommandProcessor;
+import com.zoomfolks.tidsoptimist_bot.bot.publisher.BotMessagePublisher;
+import com.zoomfolks.tidsoptimist_bot.config.BotConfigurationProperties;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static com.zoomfolks.tidsoptimist_bot.utils.CommandUtils.getChatId;
+
+@Service
+public class PongCommandProcessor extends AbstractCommandProcessor {
+    protected PongCommandProcessor(BotConfigurationProperties botConfigurationProperties, BotMessagePublisher applicationEventPublisher) {
+        super(botConfigurationProperties, applicationEventPublisher);
+    }
+
+    @Override
+    protected void doProcess(Update update) {
+        botMessagePublisher.publishMessage(new SendMessage(getChatId(update), "хуйонг"));
+    }
+
+    @Override
+    public String getCommand() {
+        return "pong";
+    }
+}
---
+++ src/main/java/com/zoomfolks/tidsoptimist_bot/bot/command/processor/late/LateCommandChainHandler.java
@@ -0,0 +1,6 @@
+package com.zoomfolks.tidsoptimist_bot.bot.command.processor.late;
+
+public interface LateCommandChainHandler {
+
+
+}
