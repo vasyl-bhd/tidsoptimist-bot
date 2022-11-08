29c85063aac27f661633d124645c7222b3dee86d
vasyl.bhd <vasyl.bhd@gmail.com>
2022-11-08 23:28:59 +0200

    WIll do better next time

--- src/main/java/com/zoomfolks/tidsoptimist_bot/bot/command/processor/speech/handler/RandomMemeAnswerHandler.java
+++ src/main/java/com/zoomfolks/tidsoptimist_bot/bot/command/processor/speech/handler/RandomMemeAnswerHandler.java
@@ -1,18 +1,24 @@
 package com.zoomfolks.tidsoptimist_bot.bot.command.processor.speech.handler;
 
+import com.zoomfolks.tidsoptimist_bot.bot.publisher.BotMessagePublisher;
 import com.zoomfolks.tidsoptimist_bot.bot.service.MemeGenerationHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static com.zoomfolks.tidsoptimist_bot.utils.CommandUtils.getChatId;
 
 @Service
 @RequiredArgsConstructor
 public class RandomMemeAnswerHandler implements Handler {
 
-private final MemeGenerationHelper helper;
+    private final MemeGenerationHelper helper;
+    private final BotMessagePublisher botMessagePublisher;
 
     @Override
     public void handle(Update update) {
-       helper.help(update);
+        botMessagePublisher.publishMessage(new SendMessage(getChatId(update), "RANDOM MEME GOGOGOG"));
+        helper.help(update);
     }
 }
