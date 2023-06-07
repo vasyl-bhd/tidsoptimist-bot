6863fec0816b0e138a010458e356e6979c57bb00
vasyl.bhd <vasyl.bhd@gmail.com>
2023-06-07 20:03:17 +0300

    Add job for PR remind

---
+++ src/main/java/com/zoomfolks/tidsoptimist_bot/scheduler/PrCheckReminder.java
@@ -0,0 +1,64 @@
+package com.zoomfolks.tidsoptimist_bot.scheduler;
+
+import com.zoomfolks.tidsoptimist_bot.bot.publisher.BotMessagePublisher;
+import com.zoomfolks.tidsoptimist_bot.config.BotConfigurationProperties;
+import com.zoomfolks.tidsoptimist_bot.scheduler.dto.Answer;
+import com.zoomfolks.tidsoptimist_bot.utils.ListUtils;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.zoomfolks.tidsoptimist_bot.utils.ListUtils.getRandomElement;
+
+@Service
+public class PrCheckReminder {
+
+    private static final List<String> questionVariations = List.of(
+            "Hey folks, could you give the open PRs some love today?",
+            "Howdy team, the open PRs are feeling a bit lonely. Could you check them out when you have a moment?",
+            "Dear team, your attention to the open PRs would be greatly appreciated.",
+            "Team, the open PRs are getting more unread than my weekend emails. Please give them a look.",
+            "Hey all, those open PRs won't review themselves...or will they? Until we find out, could you please check them out?",
+            "Hello everyone, the open PRs are having a party today. Sadly, they didn't invite us. Shall we crash it?"
+    );
+
+    private static final Map<Answer, List<String>> answerVariations = Map.of(
+            Answer.Yes,  List.of("Yes, I've reviewed a few PRs today and I'll get to the rest soon.",
+                    "Absolutely, mate! The PRs and I are already having a chat.",
+                    "Indeed, the PRs and I had a wonderful date this morning.",
+                    "Yep",
+                    "Y"
+                    ),
+            Answer.NotYet, List.of("I regret to inform you that I have not yet reviewed the open PRs.",
+                    "Sorry, my PR review ride was delayed due to an unexpected coffee spill.",
+                    "I regret to inform you that I have not yet reviewed the open PRs."),
+            Answer.No, List.of( "Oops, didn't manage to get to those PRs today, my bad!",  "No, I didn't get a chance to review the PRs today.", "Nope, our PR friends and I didn't get to hang out today.",  "Sadly, the PRs were left alone, dreaming of my return."),
+            Answer.Ignoring, List.of( "I haven't reviewed the PRs, and I don't plan to do it today.", "Between you and me, I've been giving those PRs a bit of a cold shoulder today.", "I have consciously deferred the review of the open PRs.", "The PRs are in my 'for-later' pile, also known as the 'probably-never' pile.")
+    );
+
+    private final BotMessagePublisher botMessagePublisher;
+    private final long groupId;
+
+    public PrCheckReminder(BotMessagePublisher botMessagePublisher, BotConfigurationProperties botConfigurationProperties) {
+        this.botMessagePublisher = botMessagePublisher;
+        groupId = botConfigurationProperties.getGroupId();
+    }
+
+    @Scheduled(cron = "0 14 * * MON-FRI")
+    public void sendCheckPrReminder() {
+        var options = answerVariations.values()
+                .stream()
+                .map(ListUtils::getRandomElement)
+                .collect(Collectors.toList());
+
+        var sendPoll = new SendPoll(String.valueOf(groupId), getRandomElement(questionVariations),
+                options);
+
+        botMessagePublisher.publishMessage(sendPoll);
+    }
+
+}
---
+++ src/main/java/com/zoomfolks/tidsoptimist_bot/scheduler/dto/Answer.java
@@ -0,0 +1,5 @@
+package com.zoomfolks.tidsoptimist_bot.scheduler.dto;
+
+public enum Answer {
+    Yes,NotYet,No,Ignoring
+}
