11f55dabb208c04f66b1e23915be88f2c3e5ccbc
vasyl.bhd <vasyl.bhd@gmail.com>
2023-06-26 19:39:09 +0300

    Add backup plan

--- src/main/java/com/zoomfolks/tidsoptimist_bot/bot/command/processor/WakieWakieCommandProcessor.java
+++ src/main/java/com/zoomfolks/tidsoptimist_bot/bot/command/processor/WakieWakieCommandProcessor.java
@@ -6,6 +6,7 @@ import com.zoomfolks.tidsoptimist_bot.bot.publisher.BotMessagePublisher;
 import com.zoomfolks.tidsoptimist_bot.config.BotConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
 import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
 import org.telegram.telegrambots.meta.api.objects.EntityType;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
@@ -15,13 +16,17 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import java.util.List;
 
 import static com.zoomfolks.tidsoptimist_bot.utils.CommandUtils.getChatId;
+import static com.zoomfolks.tidsoptimist_bot.utils.ListUtils.getRandomElement;
 import static com.zoomfolks.tidsoptimist_bot.utils.MessageUtils.getUserNames;
 import static com.zoomfolks.tidsoptimist_bot.utils.MessageUtils.sendTyping;
 
 @Service
 public class WakieWakieCommandProcessor extends AbstractCommandProcessor {
+
+    private final String botName;
     protected WakieWakieCommandProcessor(BotConfigurationProperties botConfigurationProperties, BotMessagePublisher applicationEventPublisher) {
         super(botConfigurationProperties, applicationEventPublisher);
+         this.botName = botConfigurationProperties.getUsername();
     }
 
     @Override
@@ -37,7 +42,14 @@ public class WakieWakieCommandProcessor extends AbstractCommandProcessor {
 
         var userNames = getUserNames(messageEntities);
 
-        System.out.println(userNames);
+        if (userNames.isEmpty()) {
+            botMessagePublisher.publishMessage(new SendMessage(chatId, "Bruh, tag someone (e.g. @tidsoptimist_bot)"));
+        }
+
+        if (userNames.size() == 1 && userNames.contains("@" + botName)) {
+            botMessagePublisher.publishMessage(new SendMessage(chatId, "Not me, duuuh"));
+            return;
+        }
 
         var sendVideo = SendVideo.builder()
                 .chatId(chatId)
